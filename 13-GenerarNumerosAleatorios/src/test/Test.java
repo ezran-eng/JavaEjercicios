4541e6a6bc2a2fa0cf433042836943c5cff789a4
eze <62077267+ezran-eng@users.noreply.github.com>
2021-11-01 17:47:55 -0300

    Add files via upload

---
+++ 13-GenerarNumerosAleatorios/src/test/Test.java
@@ -0,0 +1,21 @@
+/*
+    Genera 10 numeros aleatorios entre 1 y 10.
+*/
+package test;
+
+
+public class Test {
+    public static void main(String[] args) {
+        
+        int maximo = 10;
+        int minimo = 1;
+        
+        int aleatorio;
+        
+        for (int i = 0; i < 10; i++) {
+            aleatorio = (int)(Math.random() * (maximo - minimo + 1) + (minimo));
+            System.out.println(aleatorio);
+        }
+    }
+    
+}
